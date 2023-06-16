9a583a64d2473110f72e9822d7096f4506ff7b27
KingMose007 <orshimoses7@gmail.com>
2023-06-16 11:54:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
 
     return (0);
-i}
+}
