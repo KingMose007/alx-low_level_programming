b91315482672b6640da07c442f3715178e0e687d
KingMose007 <orshimoses7@gmail.com>
2023-06-20 18:08:00 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints every natural numbers n to 98
+ * @n: The starting number
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+			printf("%d, ", i);
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
+	}
+	printf("98\n");
+}
