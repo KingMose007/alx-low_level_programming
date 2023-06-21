7e5554508c56d37449bd5ced3828c576bf13914d
KingMose007 <orshimoses7@gmail.com>
2023-06-20 18:13:29 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,43 +8,37 @@
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int result;
+	int i, j;
 
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar('0');
+
+			for (j = 1; j <= n; j++)
 			{
-				result = i * j;
-
-				if (j == 0)
-					_putchar('0');
-				else if (result < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-				}
-				else if (result < 100)
-				{
-					_putchar(' ');
-					_putchar(result / 10 + '0');
-					_putchar(result % 10 + '0');
-				}
-				else
-				{
-					_putchar(result / 100 + '0');
-					_putchar(result / 10 % 10 + '0');
-					_putchar(result % 10 + '0');
-				}
-
-				if (j != n)
+				if ((i * j) < 10)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + i * j);
+				}
+				else if ((i * j) >= 10 && (i * j) < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (i * j) / 10);
+					_putchar('0' + (i * j) % 10);
+				}
+				else if ((i * j) >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (i * j) / 100);
+					_putchar('0' + ((i * j) / 10) % 10);
+					_putchar('0' + (i * j) % 10);
 				}
 			}
 
