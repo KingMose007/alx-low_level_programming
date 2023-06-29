df348f5adf0f7e1c63cd01b873366779c7994580
KingMose007 <orshimoses7@gmail.com>
2023-06-29 15:25:31 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,21 +2,31 @@
 #include <stdlib.h>
 
 /**
- * cap_string - function that capitalizes all words of a string
+ * cap_string - it does capitalizes all words of a string
  * @s: string to be modified
  *
  * Return: pointer to the modified string
  */
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
+	char separators[] = " \t\n,;.!?\"(){}";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 'a' + 'A';
-		else if (s[i - 1] == ' ' && s[i] >= 'a' && s[i] <= 'z')
-			s[i] = s[i] - 'a' + 'A';
+		else
+		{
+			for (j = 0; separators[j] != '\0'; j++)
+			{
+				if (s[i - 1] == separators[j] && s[i] >= 'a' && s[i] <= 'z')
+				{
+					s[i] = s[i] - 'a' + 'A';
+					break;
+				}
+			}
+		}
 	}
 
 	return (s);
