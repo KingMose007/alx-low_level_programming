64e590a528e7f8ab64ead43480ee89715098709b
KingMose007 <orshimoses7@gmail.com>
2023-07-05 15:18:47 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,46 +1,19 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c
- * @c: The character to print
+ * _print_rev_recursion - prints string reverse
+ * @s: the string to reverse
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * return: nothing.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * _strlen_recursion - returns the length of a string
- * @s: string to find the length of
- *
- * Return: length of string
- */
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
-}
-
-/**
- * print_string_rev - prints a string in reverse
- * @str: the string to print
- *
- * Return: void
- */
-void print_string_rev(char *str)
-{
-	int len = _strlen_recursion(str);
-
-	if (len > 0)
 	{
-		_putchar(*(str + len - 1));
-		*(str + len - 1) = '\0';
-		print_string_rev(str);
+		return;
 	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
-
