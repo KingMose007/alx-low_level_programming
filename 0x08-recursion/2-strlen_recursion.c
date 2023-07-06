5fca91fc939db918070e2dabab26482736f8eb76
KingMose007 <orshimoses7@gmail.com>
2023-07-06 07:22:31 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,16 +4,16 @@
  * _strlen_recursion - return length of string
  * @s: the string to count
  *
- * return: integer value
+ * Return: integer value
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 	else
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
