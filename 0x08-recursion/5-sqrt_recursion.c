e44fe74c7c948776d87c60068d244ce1ee0cf803
KingMose007 <orshimoses7@gmail.com>
2023-07-06 07:48:20 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,41 @@
 #include "main.h"
-#include <stdio.h>
-
-int check_prime(int n, int i);
 
 /**
- *i
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to calculate the square root of
+ *
+ * Return: the natural square root of n, or -1 if n does not have a natural
+ * square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (_sqrt_helper(n, 0));
+}
+
+/**
+ * _sqrt_helper - helper function to find the square root of n
+ * @n: the number to calculate the square root of
+ * @i: the current number to test as the square root of n
+ *
+ * Return: the natural square root of n, or -1 if n does not have a natural
+ * square root
+ */
+int _sqrt_helper(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	else if (i * i == n)
+	{
+		return (i);
+	}
+	else
+	{
+		return (_sqrt_helper(n, i + 1));
+	}
+}
