13aa35e17cd3ce6e58cac0e2b65f2bc782875f3f
KingMose007 <orshimoses7@gmail.com>
2023-07-19 16:20:51 -0700

    function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef function_pointers.h
+#define function_pointers.h
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
