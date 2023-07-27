6e9b4bf4884deedc00e37e875be1bacfaa326642
KingMose007 <orshimoses7@gmail.com>
2023-07-27 16:07:43 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-section .data
-msg db 'Hello, Holberton', 0Ah, 0
-
-section .text
-global _start
-
-_start:
-; write message to stdout
-mov rax, 1          ; system call for write
-mov rdi, 1          ; file descriptor for stdout
-mov rsi, msg        ; message to write
-mov rdx, 17         ; length of message
-syscall
-
-; exit program
-mov rax, 60         ; system call for exit
-xor rdi, rdi        ; exit status code
-syscall
+global    main
+extern    printf
+main:
+mov   edi, format
+xor   eax, eax
+call  printf
+mov   eax, 0
+ret
+format: db Hello, Holberton\n,0
