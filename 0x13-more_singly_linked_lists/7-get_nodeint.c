12105fd280c050772ecc7292b57a434a97e51f07
KingMose007 <orshimoses7@gmail.com>
2023-08-01 14:23:01 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,7 @@
  * @head: pointer to the head of the list
  * @index: index of the node to return, starting at 0
  *
- * Return: the nth node of a listint_t linked list, or NULL if it does not exist
+ * Return: the nth node of listint_t linked list, or NULL if it does not exist
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
